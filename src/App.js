56f063abd5a564f547cbc7bfb4216942163ca29c
Sourabhpdnker <137615880+Sourabhpdnker@users.noreply.github.com>
2023-10-21 12:19:10 +0530

    some chandes in app adding props

--- src/App.js
+++ src/App.js
@@ -1,68 +1,18 @@
 // import logo from './logo.svg';
+
+import Navbar from './components/Navbar';
 import './App.css';
+import TextForm from './components/TextForm';
 
 function App() {
   return ( 
     <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Texttutales</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link active dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            fitures
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" aria-disabled="true">About</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+    <Navbar/>
+    <div className='container my-3'>
+      <TextForm experiment ="Do something"  title = "Enter The Text To Analyze Below."/>
     </div>
-  </div>
-</nav>
     </>
-    // <div classNameName="App">
-    //   { <header classNameNameName="App-header">
-    //     <img src={logo} classNameName="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       classNameName="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React with Sourabh
-    //     </a>
-    //   </header> }
-    // </div>
   );
 }
 
 export default App;
-// var React = require("react");
-// var ReactDom = require("react-dom");
-
-// ReactDom.render(<h1>Jay Shri ram .. </h1>,document.getElementById("root"));
-
-// var h1 = document.createElement("h1");
-// h1.innerHTML = "hello sourabh ";
-// document.getElementById("root").appendChild(h1);
