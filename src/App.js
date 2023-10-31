e421a13ea24b76d60e3dd262147eb32c8541baf0
Sourabhpdnker <137615880+Sourabhpdnker@users.noreply.github.com>
2023-11-01 01:32:42 +0530

    routers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 // import logo from './logo.svg';
-
 import Navbar from './components/Navbar';
 import './App.css';
+import About from './components/About';
 import TextForm from './components/TextForm';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+}from 'react-router-dom';
 
 function App() {
+
   return ( 
-    <>
-    <Navbar/>
-    <div className='container my-3'>
-      <TextForm experiment ="Do something"  title = "Enter The Text To Analyze Below."/>
+    <Router>
+      <div className='App content'>
+        <Navbar/>
+    <div className='container my-3 content'>
+      <Routes>
+          <Route path="/About" element={<About/>}/>
+          <Route exact path="/" element={ <TextForm experiment ="Do something"  title = "Enter The Text To Analyze Below."/>}/>
+      </Routes>
     </div>
-    </>
+    </div>
+    </Router>
   );
 }
 
