96b289154623f3612fa4a88c67075167de8db39e
Sourabhpdnker <137615880+Sourabhpdnker@users.noreply.github.com>
2023-11-01 01:26:22 +0530

    navbar , file deleted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useState, useEffect } from 'react';
+import Alert from './Alert';
+import { Link } from 'react-router-dom';
 
-export default function Navbar() {
+
+export default function Navbar(props) {
+  const [darkMode,setDarkMode] = useState(false);//whether dark mode is enableed or not 
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark-mode');
+      showAlert(" Dark Mode" , "Success");
+    }
+    else {
+      document.body.classList.remove('dark-mode');
+      showAlert(" Light Mode" , "Success");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  }
+  const [alert , setAlert] = useState(null);
+
+  const showAlert = (massage, type) => {
+    setAlert({
+      msg: massage,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <>
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
     <div className="container-fluid">
-      <a className="navbar-brand" href="/">Texttutales</a>
+      <Link className="navbar-brand" to="/">Texttutales</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -26,16 +58,26 @@ export default function Navbar() {
             </ul>
           </li>
           <li className="nav-item">
-            <a className="nav-link active" aria-disabled="true">About</a>
+          <Link className="nav-link active" aria-current="page" to="/About">About</Link>
+          </li>
+          <li className="nav-item">
+          {/* <button className="btn btn-outline-success" onClick={toggleDarkMode} type="submit">Dark mode</button> */}
+          
           </li>
         </ul>
         <form className="d-flex" role="search">
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
           <button className="btn btn-outline-success" type="submit">Search</button>
         </form>
+        <div class="form-check form-switch text-light mx-4 my-3">
+          <input class="form-check-input" onClick={toggleDarkMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+          <label class="form-check-label" htmlFor="flexSwitchCheckDefault">Dark Mode </label>
+          </div>
       </div>
     </div>
   </nav>
+  <Alert alert={alert}></Alert>
+  </>
   )
 }
 
